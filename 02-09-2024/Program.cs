a035de8250aaeb50de7e7d57cf3c5b8f3436012d
Nehaeliza <nehamaya2923@gmail.com>
2024-09-03 08:47:10 +0530

    Create Program.cs

---
+++ 02-09-2024/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week4task2.task2;
+
+namespace week4task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AllergyMenu.Menu();
+            Console.ReadKey();
+        }
+    }
+}
